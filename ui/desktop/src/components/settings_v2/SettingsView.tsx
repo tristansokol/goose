2743edd27868d60cf35bbfebb08970ac7da47afb
Lily Delalande <ldelalande@squareup.com>
2025-03-26 21:31:27 -0400

    remove log

--- ui/desktop/src/components/settings_v2/SettingsView.tsx
+++ ui/desktop/src/components/settings_v2/SettingsView.tsx
@@ -19,8 +19,6 @@ export default function SettingsView({
   setView: (view: View) => void;
   viewOptions: SettingsViewOptions;
 }) {
-  // constantly re=renders
-  console.log('endless logs');
   return (
     <div className="h-screen w-full">
       <div className="relative flex items-center h-[36px] w-full bg-bgSubtle"></div>
